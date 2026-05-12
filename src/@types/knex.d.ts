050d2515a53e3087b209e433fd3f511a139133d2
ArthurFloro <arthur.floro2006@gmail.com>
2026-05-12 19:39:38 -0300

    feat: preparando projeto para deploy

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,12 +1,12 @@
 import { Knex } from 'knex'
 
 declare module 'knex/types/tables' {
-  export interface tables {
-    transactions: {
-      id: string
-      title: string
-      amount: number
-      created_at: string
+    export interface tables {
+        transactions: {
+            id: string
+            title: string
+            amount: number
+            created_at: string
+        }
     }
-  }
 }
