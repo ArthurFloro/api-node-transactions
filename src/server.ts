7d47ea10bfdc1df639ea4d58a1d8c5e4d29bf761
ArthurFloro <arthur.floro2006@gmail.com>
2026-05-12 20:19:32 -0300

    feat: ajustando mais uma dependencia do render

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { env } from './env/index.js'
 app
   .listen({
     host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
+    port: env.PORT
   })
   .then(() => {
     console.log('HTTP Server Running')
